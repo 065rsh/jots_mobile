9b934c255204e7dd2370d0e92f9ee2cce7927536
065rsh <gogl.arshdeep@gmail.com>
2020-05-10 23:27:58 +0530

    Initial commit | Custom log in design

---
+++ lib/theme.dart
@@ -0,0 +1,45 @@
+/**
+ * Creating custom color palettes is part of creating a custom app. The idea is to create
+ * your class of custom colors, in this case `CompanyColors` and then create a `ThemeData`
+ * object with those colors you just defined.
+ *
+ * Resource:
+ * A good resource would be this website: http://mcg.mbitson.com/
+ * You simply need to put in the colour you wish to use, and it will generate all shades
+ * for you. Your primary colour will be the `500` value.
+ *
+ * Colour Creation:
+ * In order to create the custom colours you need to create a `Map<int, Color>` object
+ * which will have all the shade values. `const Color(0xFF...)` will be how you create
+ * the colours. The six character hex code is what follows. If you wanted the colour
+ * #114488 or #D39090 as primary colours in your theme, then you would have
+ * `const Color(0x114488)` and `const Color(0xD39090)`, respectively.
+ *
+ * Usage:
+ * In order to use this newly created theme or even the colours in it, you would just
+ * `import` this file in your project, anywhere you needed it.
+ * `import 'path/to/theme.dart';`
+ */
+
+import 'package:flutter/material.dart';
+
+final lightBorderColor = CompanyColors.lightBorderCC;
+final linkColor = CompanyColors.blue;
+final darkTextColor = CompanyColors.dark55;
+
+// final ThemeData CompanyThemeData = new ThemeData(
+//   brightness: Brightness.light,
+//   primarySwatch: CompanyColors.blue,
+// );
+
+class CompanyColors {
+  CompanyColors._(); // this basically makes it so you can instantiate this class
+  static const blue = Color(0xFF2382FF);
+  static const dark55 = Color(0xFF555555);
+  static const lightBorderCC = Color(0xFFCCCCCC);
+
+  // static const Map<int, Color> blue = const <int, Color>{
+  //   50: const Color(/* some hex code */),
+  // };
+
+}
