8e89cc691730d1cb8d6871d33e5079f54cadfed2
065rsh <gogl.arshdeep@gmail.com>
2020-05-16 00:45:02 +0530

    Show display picture

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,27 +1,111 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:jots_mobile/services/auth.dart';
+import 'package:provider/provider.dart';
 
-class Home extends StatelessWidget {
-  final AuthService _auth = AuthService();
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String imageUrl;
 
   @override
   Widget build(BuildContext context) {
+    var user = Provider.of<FirebaseUser>(context);
+
     return Scaffold(
       backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text("Home"),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () async {
-              await _auth.signOut();
-            },
-            label: Text("Log out"),
-            icon: Icon(Icons.person),
-          ),
-        ],
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  user.photoUrl,
+                ),
+                radius: 30,
+                backgroundColor: Colors.transparent,
+              ),
+            ),
+            HomePage(),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Offset> _offsetAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    _offsetAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0, 0.5),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+        position: _offsetAnimation,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 20,
+                offset: Offset(3, 7),
+              ),
+            ],
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.topRight,
+                child: FlatButton(
+                  onPressed: () {
+                    _controller.forward();
+                  },
+                  child: Image.asset(
+                    "assets/images/DownArrow.png",
+                    width: 25,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
